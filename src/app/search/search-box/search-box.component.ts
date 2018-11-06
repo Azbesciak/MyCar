af8439b937192fdd0a63881e03076a9aec084ad6
Witold Kupś <witold.kups@interlan.pl>
2018-11-06 21:40:33 +0100

    Add grid to search-box

--- src/app/search/search-box/search-box.component.ts
+++ src/app/search/search-box/search-box.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBoxComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
--- src/app/search/search-box/search-input/search-input.component.ts
+++ src/app/search/search-box/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
@@ -11,11 +11,15 @@ import {map, startWith} from 'rxjs/operators';
 export class SearchInputComponent implements OnInit {
 
   myControl = new FormControl();
-  options: string[] = ['One', 'Two', 'Three'];
-  filteredOptions: Observable<string[]>;
+  @Input()
+  title: string;
+
+  @Input()
+  possibleValues: string[] = [];
+  suggestions: Observable<string[]>;
 
   ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
+    this.suggestions = this.myControl.valueChanges.pipe(
       startWith(''),
       map(value => this._filter(value))
     );
@@ -24,7 +28,8 @@ export class SearchInputComponent implements OnInit {
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
 
-    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+    return this.possibleValues
+      .filter(option => option.toLowerCase().indexOf(filterValue) === 0);
   }
 
 }
