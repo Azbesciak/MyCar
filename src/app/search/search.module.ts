7f38f5797e6d72db07ff25a667fbfb3109e92564
Witold Kupś <witold.kups@interlan.pl>
2018-12-11 22:18:16 +0100

    Add example carousel

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -3,13 +3,15 @@ import {CommonModule} from '@angular/common';
 import {SearchComponent} from './search.component';
 import {SearchRoutingModule} from './search-routing.module';
 import {SearchBoxModule} from './search-box/search-box.module';
+import {CarouselModule} from '../carousel/carousel.module';
 
 @NgModule({
   declarations: [SearchComponent],
   imports: [
     CommonModule,
     SearchRoutingModule,
-    SearchBoxModule
+    SearchBoxModule,
+    CarouselModule
   ]
 })
 export class SearchModule {
